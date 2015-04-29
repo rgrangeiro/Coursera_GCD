4bbe725fc88021b02f699b84ff85b16d6fc236fd
Robson Grangeiro <robson.grangeiro@gmail.com>
2015-04-28 22:29:30 -0300

    week4 files

---
+++ week4/scripts.R
@@ -0,0 +1,28 @@
+acast()
+dcast()
+sapply() - divide um DT e aplica uma função
+
+pacote dplyr
+  arrange()
+    arrange(join())
+      join_all()
+  filter()
+  select()
+  mutate()
+  rename()
+merge()
+intersect()
+
+##
+
+tolower()
+toupper()
+strsplit()
+sub()
+gsub()
+greep()
+greepl()
+substr()
+paste()
+paste0()
+str_trim()
